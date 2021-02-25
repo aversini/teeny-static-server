b21bf1548ef06be4a6712ded59d23eb0160a46e6
Arno Versini <arno.versini@wellsfargo.com>
2021-02-24 17:12:56 -0800

    chore: bump bump-and-release to latest

--- .bump-and-release.config.js
+++ .bump-and-release.config.js
@@ -2,24 +2,39 @@ module.exports = {
   bump: {
     nextPossible: [
       {
+        pos: 0,
+        type: "prepatch",
+        identifier: "beta",
+        prompt: (type, version) =>
+          `[${type}] ..... marking current as beta (${version})`,
+        default: true,
+      },
+      {
+        pos: 1,
         type: "patch",
         prompt: (type, version) =>
-          `[${type}] ... only defect fixes (${version})`,
+          `[${type}] ........ only defect fixes (${version})`,
       },
       {
+        pos: 3,
         type: "minor",
-        default: true,
         prompt: (type, version) =>
-          `[${type}] ... at least one new feature (${version})`,
+          `[${type}] ........ at least one new feature (${version})`,
       },
       {
+        pos: 4,
         type: "major",
         prompt: (type, version) =>
-          `[${type}] ... breaking change(s) (${version})`,
+          `[${type}] ........ breaking change(s) (${version})`,
       },
       {
+        pos: 5,
+        type: "separator",
+      },
+      {
+        pos: 6,
         type: "custom",
-        prompt: (type) => `[${type}] .. enter a custom version`,
+        prompt: (type) => `[${type}] ....... enter a custom version`,
       },
     ],
   },
